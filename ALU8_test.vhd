0476163525963ea06cdd8529b0834abc0f3445e2
William Ng <165541107+willofcode@users.noreply.github.com>
2024-06-23 12:26:45 -0400

    Upload Coursework
    Upload 8-Bit ALU implementation

---
+++ ALU8_test.vhd
@@ -0,0 +1,100 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   14:51:58 06/17/2024
+-- Design Name:   
+-- Module Name:   C:/Users/cs343/Desktop/Lab_01/ALU8_test.vhd
+-- Project Name:  Lab_01
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: ALU8
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY ALU8_test IS
+END ALU8_test;
+ 
+ARCHITECTURE behavior OF ALU8_test IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT ALU8
+    PORT(
+         A : IN  std_logic_vector(7 downto 0);
+         A_inv : IN  std_logic_vector(7 downto 0);
+         B : IN  std_logic_vector(7 downto 0);
+         B_inv : IN  std_logic_vector(7 downto 0);
+         CarryIn : IN  std_logic_vector(7 downto 0);
+         OP : IN  std_logic_vector(1 downto 0);
+         CarryOut : OUT  std_logic_vector(7 downto 0);
+         Result : OUT  std_logic_vector(7 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal A : std_logic_vector(7 downto 0) := (others => '0');
+   signal A_inv : std_logic_vector(7 downto 0) := (others => '0');
+   signal B : std_logic_vector(7 downto 0) := (others => '0');
+   signal B_inv : std_logic_vector(7 downto 0) := (others => '0');
+   signal CarryIn : std_logic_vector(7 downto 0) := (others => '0');
+   signal OP : std_logic_vector(7 downto 0) := (others => '0');
+
+ 	--Outputs
+   signal CarryOut : std_logic_vector(7 downto 0);
+   signal Result : std_logic_vector(7 downto 0);
+
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: ALU8 PORT MAP (
+          A => A,
+          A_inv => A_inv,
+          B => B,
+          B_inv => B_inv,
+          CarryIn => CarryIn,
+          OP => OP,
+          CarryOut => CarryOut,
+          Result => Result
+        );
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+		-- A AND B
+		OP <= "00"; CarryIn <= '0'; A_inv <= '0'; B_inv <= '0';
+		A <= "01111000"; B <= "00011110"; wait for 40 ns;
+		-- A OR B
+		OP <= "01"; CarryIn <= '0'; A_inv <= '0'; B_inv <= '0';
+		A <= "01111000"; B <= "00011110"; wait for 40 ns;
+		-- A + B
+		OP <= "10"; CarryIn <= '0'; A_inv <= '0'; B_inv <= '0';
+		A <= "00001100"; B <= "00111000"; wait for 40 ns;
+		-- A - B
+		OP <= "10"; CarryIn <= '1'; A_inv <= '0'; B_inv <= '1';
+		A <= "01010101"; B <= "00101010"; wait for 40 ns;
+      wait;
+   end process;
+
+END;
---
+++ ALU_8bit.vhd
@@ -0,0 +1,52 @@
+----------------------------------------------------------------------------------
+-- Company: CCNY
+-- Engineer: William Ng
+-- 
+-- Create Date:    17:16:00 06/16/2024 
+-- Design Name: 	8-Bit ALU test
+-- Module Name:    ALU8 - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity ALU8 is
+    Port ( A :       in  STD_LOGIC_VECTOR (7 downto 0);
+           A_inv :   in  STD_LOGIC;
+           B :       in  STD_LOGIC_VECTOR (7 downto 0);
+           B_inv :   in  STD_LOGIC;
+           CarryIn : in  STD_LOGIC;
+           OP :      in  STD_LOGIC_VECTOR (1 downto 0);
+           CarryOut :out  STD_LOGIC;
+           Result :  out  STD_LOGIC_VECTOR (7 downto 0));
+end ALU8;
+
+architecture ALU8_arch of ALU8 is
+signal CO_01, CO_02, CO_03, CO_04, CO_05, CO_06, CO_07: STD_LOGIC;
+component ALU 
+    Port ( A, A_inv, B, B_inv, CarryIn, OP0, OP1: in STD_LOGIC;
+           CarryOut, Result : out STD_LOGIC); 
+end component;
+begin
+    U1: ALU port map( A(0), A_inv, B(0), B_inv, CarryIn, OP(0), OP(1), CO_01, Result(0));
+    U2: ALU port map( A(1), A_inv, B(1), B_inv, CO_01, OP(0), OP(1), CO_02, Result(1));
+    U3: ALU port map( A(2), A_inv, B(2), B_inv, CO_02, OP(0), OP(1), CO_03, Result(2));
+    U4: ALU port map( A(3), A_inv, B(3), B_inv, CO_03, OP(0), OP(1), CO_04, Result(3));
+    U5: ALU port map( A(4), A_inv, B(4), B_inv, CO_04, OP(0), OP(1), CO_05, Result(4));
+    U6: ALU port map( A(5), A_inv, B(5), B_inv, CO_05, OP(0), OP(1), CO_06, Result(5));
+    U7: ALU port map( A(6), A_inv, B(6), B_inv, CO_06, OP(0), OP(1), CO_07, Result(6));
+    U8: ALU port map( A(7), A_inv, B(7), B_inv, CO_07, OP(0), OP(1), CarryOut, Result(7));
+end ALU8_arch;
+
