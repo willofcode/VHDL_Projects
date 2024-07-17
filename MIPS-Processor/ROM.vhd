6720a6c2c598a3773d313daa406bbc81ea16b1bf
William Ng <165541107+willofcode@users.noreply.github.com>
2024-07-17 02:01:32 -0400

    Create ROM.vhd

---
+++ MIPS-Processor/ROM.vhd
@@ -0,0 +1,45 @@
+----------------------------------------------------------------------------------
+-- Company: CCNY	
+-- Engineer: GROUP 1
+-- 
+-- Create Date:    23:48:10 07/16/2024 
+-- Design Name: 
+-- Module Name:    ROM - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ROM is
+    Port ( I_ROM_EN : in  STD_LOGIC;
+           I_ROM_ADDR : in  STD_LOGIC_VECTOR (31 downto 0);
+           O_ROM_DATA : out  STD_LOGIC_VECTOR (31 downto 0));
+end ROM;
+
+architecture Behavioral of ROM is
+
+begin
+
+
+end Behavioral;
+
