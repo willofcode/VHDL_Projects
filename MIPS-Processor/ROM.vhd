814bc66c9ecb07b56b31824bb329663a4692d871
William Ng <165541107+willofcode@users.noreply.github.com>
2024-07-17 03:43:27 -0400

    Upload ROM

--- MIPS-Processor/ROM.vhd
+++ MIPS-Processor/ROM.vhd
@@ -37,9 +37,26 @@ entity ROM is
 end ROM;
 
 architecture Behavioral of ROM is
-
+    type ROM_Array is array (0 to 255) of STD_LOGIC_VECTOR(31 downto 0);
+    signal ROM_Content : ROM_Array := (
+        0 => "00100000000110010010000000000000",
+        1 => "00100111001110010000000000000001",
+        2 => "00000001001010100101100000100001",
+        3 => "00100100000010010000000000000001",
+        4 => "00010010000100101111111111111011",
+        5 => "10001111001010000000000000000001",
+        6 => "00010101101011001111111111111001",
+        7 => "10101101001010001111111111111111",
+        8 => "00001000000000000000000000001001",
+        9 => "00100100000000100000000000001010",
+        10 => "00000000000000000000000000001100",
+        others => "00000000000000000000000000000000"
+    );
 begin
-
-
+    process (I_ROM_EN, I_ROM_ADDR)
+    begin
+        if I_ROM_EN = '1' then
+            O_ROM_DATA <= ROM_Content(to_integer(unsigned(I_ROM_ADDR(7 downto 0))));
+        end if;
+    end process;
 end Behavioral;
-
