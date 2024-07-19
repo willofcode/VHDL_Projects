93682104610fef834d07cc94a9ec8cc83b76b31b
William Ng <165541107+willofcode@users.noreply.github.com>
2024-07-19 02:09:52 -0400

    Update ROM

--- MIPS-Processor/ROM.vhd
+++ MIPS-Processor/ROM.vhd
@@ -1,8 +1,8 @@
 ----------------------------------------------------------------------------------
--- Company: CCNY	
+-- Company: CCNY
 -- Engineer: GROUP 1
 -- 
--- Create Date:    23:48:10 07/16/2024 
+-- Create Date:    14:44:56 07/17/2024 
 -- Design Name: 
 -- Module Name:    ROM - Behavioral 
 -- Project Name: 
@@ -20,6 +20,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
+use STD.textio.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -37,26 +38,42 @@ entity ROM is
 end ROM;
 
 architecture Behavioral of ROM is
-    type ROM_Array is array (0 to 255) of STD_LOGIC_VECTOR(31 downto 0);
-    signal ROM_Content : ROM_Array := (
-        0 => "00100000000110010010000000000000",
-        1 => "00100111001110010000000000000001",
-        2 => "00000001001010100101100000100001",
-        3 => "00100100000010010000000000000001",
-        4 => "00010010000100101111111111111011",
-        5 => "10001111001010000000000000000001",
-        6 => "00010101101011001111111111111001",
-        7 => "10101101001010001111111111111111",
-        8 => "00001000000000000000000000001001",
-        9 => "00100100000000100000000000001010",
-        10 => "00000000000000000000000000001100",
-        others => "00000000000000000000000000000000"
-    );
+    type ROM_Array is array (0 to 255) of STD_LOGIC_VECTOR(7 downto 0);
+    signal ROM_Content : ROM_Array;
+
+impure function init_rom(FileName : in string) return ROM_Array is
+	constant LINE_NUM : integer := 11;
+	file fp: text;
+	variable temp_rom : ROM_Array := (others => x"00");
+	variable line_cache : line;
+	variable byte_cache : bit_vector ( 31 downto 0) := x"00000000";
 begin
-    process (I_ROM_EN, I_ROM_ADDR)
+	file_open(fp, filename, read_mode);
+	for i in 0 to line_num loop
+		if endfile(fp) then 
+			exit;
+		else 
+			readline (fp, line_cache);
+			read (line_cache, byte_cache);
+			temp_rom(0+4*i) := to_stdlogicvector(byte_cache(31 downto 24));
+			temp_rom(1+4*i) := to_stdlogicvector(byte_cache(23 downto 16));
+			temp_rom(2+4*i) := to_stdlogicvector(byte_cache(15 downto 8));
+			temp_rom(3+4*i) := to_stdlogicvector(byte_cache(7 downto 0));
+		end if;
+	end loop;
+	file_close(fp);
+	return temp_rom;
+end function;
+
+begin
+    process (I_ROM_EN, I_ROM_ADDR, ROM_Content)
     begin
         if I_ROM_EN = '1' then
-            O_ROM_DATA <= ROM_Content(to_integer(unsigned(I_ROM_ADDR(7 downto 0))));
+				ROM_Content <= init_rom("ROM_init.txt");
+            O_ROM_DATA(31 downto 24) <= ROM_Content(to_integer(unsigned(I_ROM_ADDR)));
+				O_ROM_DATA(23 downto 16) <= ROM_Content(to_integer(unsigned(I_ROM_ADDR)+1));
+				O_ROM_DATA(15 downto 8) <= ROM_Content(to_integer(unsigned(I_ROM_ADDR)+2));
+				O_ROM_DATA(7 downto 0) <= ROM_Content(to_integer(unsigned(I_ROM_ADDR)+3));
         end if;
     end process;
 end Behavioral;
