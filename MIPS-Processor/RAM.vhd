b4ea67500c9db19fc6d6b0f9f5a126813569beb8
William Ng <165541107+willofcode@users.noreply.github.com>
2024-07-24 01:26:55 -0400

    Update RAM

--- MIPS-Processor/RAM.vhd
+++ MIPS-Processor/RAM.vhd
@@ -20,6 +20,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
+use STD.textio.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -40,55 +41,52 @@ entity RAM is
 end RAM;
 
 architecture Behavioral of RAM is
-    type ram_type is array (0 to 255) of std_logic_vector(31 downto 0);
-    signal RAM : ram_type := (
-        0 => "00000000000000000000000000000001",
-        1 => "00000000000000000000000000000010",
-        2 => "00000000000000000000000000000100",
-        3 => "00000000000000000000000000001000",
-        4 => "00000000000000000000000000010000",
-        5 => "00000000000000000000000000100000",
-        6 => "00000000000000000000000001000000",
-        7 => "00000000000000000000000010000000",
-        8 => "00000000000000000000000100000000",
-        9 => "00000000000000000000001000000000",
-        10 => "00000000000000000000010000000000",
-        11 => "00000000000000000000100000000000",
-        12 => "00000000000000000001000000000000",
-        13 => "00000000000000000010000000000000",
-        14 => "00000000000000000100000000000000",
-        15 => "00000000000000001000000000000000",
-        16 => "00000000000000010000000000000000",
-        17 => "00000000000000100000000000000000",
-        18 => "00000000000001000000000000000000",
-        19 => "00000000000010000000000000000000",
-        20 => "00000000000100000000000000000000",
-        21 => "00000000001000000000000000000000",
-        22 => "00000000010000000000000000000000",
-        23 => "00000000100000000000000000000000",
-        24 => "00000001000000000000000000000000",
-        25 => "00000010000000000000000000000000",
-        26 => "00000100000000000000000000000000",
-        27 => "00001000000000000000000000000000",
-        28 => "00010000000000000000000000000000",
-        29 => "00100000000000000000000000000000",
-        30 => "01000000000000000000000000000000",
-        31 => "10000000000000000000000000000000",
-        others => (others => '0')
-    );
+    type ram_type is array (0 to 8447) of std_logic_vector(7 downto 0);
+	 signal RAM : ram_type;
+	 signal temp_RAM_DATA: std_logic_vector(31 downto 0) := (others => '0');
     signal current_RAM_DATA : std_logic_vector(31 downto 0) := (others => '0');
+	 
+impure function init_ram(FileName : in string) return ram_type is
+	constant line_num : integer := 32;
+	file fp: text;
+	variable temp_ram : ram_type := (others => x"00");
+	variable line_cache : line;
+	variable byte_cache : bit_vector ( 31 downto 0) := x"00000000";
 begin
-    process(I_RAM_EN, I_RAM_RE, I_RAM_WE, I_RAM_ADDR, I_RAM_DATA)
-    begin
-        if I_RAM_EN = '1' then
+	file_open(fp, filename, read_mode);
+	for i in 0 to line_num loop
+		if endfile(fp) then 
+			exit;
+		else 
+			readline (fp, line_cache);
+			read (line_cache, byte_cache);
+			temp_ram(0+4*i) := to_stdlogicvector(byte_cache(31 downto 24));
+			temp_ram(1+4*i) := to_stdlogicvector(byte_cache(23 downto 16));
+			temp_ram(2+4*i) := to_stdlogicvector(byte_cache(15 downto 8));
+			temp_ram(3+4*i) := to_stdlogicvector(byte_cache(7 downto 0));
+		end if;
+	end loop;
+	file_close(fp);
+	return temp_ram;
+end function;
+
+begin
+    process(I_RAM_EN, I_RAM_RE, I_RAM_WE, I_RAM_ADDR, I_RAM_DATA, RAM, current_RAM_DATA)
+	 begin
+	     RAM <= init_ram("RAM_init.txt");
+        if I_RAM_EN = '1' then					
             if I_RAM_RE = '1' then
-                current_RAM_DATA <= RAM(to_integer(unsigned(I_RAM_ADDR(7 downto 0))));
-            elsif I_RAM_WE = '1' then
-                RAM(to_integer(unsigned(I_RAM_ADDR(7 downto 0)))) <= I_RAM_DATA;
+					current_RAM_DATA(31 downto 24) <= RAM(to_integer(unsigned(I_RAM_ADDR)));		-- Update when enabled
+					current_RAM_DATA(23 downto 16) <= RAM(to_integer(unsigned(I_RAM_ADDR)+1));		
+					current_RAM_DATA(15 downto 8) <= RAM(to_integer(unsigned(I_RAM_ADDR)+2));
+					current_RAM_DATA(7 downto 0) <= RAM(to_integer(unsigned(I_RAM_ADDR)+3));
+					O_RAM_DATA <= current_RAM_DATA;
+					temp_RAM_DATA <= current_RAM_DATA;
+					end if;
+				if I_RAM_WE = '1' then
+					O_RAM_DATA <= I_RAM_DATA;	
             end if;
+			else 
         end if;
     end process;
-
-    O_RAM_DATA <= current_RAM_DATA;
-
 end Behavioral;
